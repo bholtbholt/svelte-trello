fa9652e389a3729cfb31c97fa1b40ec9ab9d33ab
Brian Holt <bholtbholt@icloud.com>
2023-09-20 18:32:47 -0700

    Setup trello (#1)
    * Remove scaffolded components
    
    * Add aliases
    
    * Build
    
    * WIP
    
    * WIP

---
+++ components/Member.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let className = '';
+  export { className as class };
+
+  export let id;
+  export let name;
+  export let pic;
+  export let bgColor = 'bg-gray-200';
+  export let textColor = 'text-black';
+</script>
+
+<span
+  class="{className}
+    px-2 py-1 rounded
+    inline-block tracking-tightest
+    {bgColor} {textColor}"
+>
+  {pic}
+  {name}
+</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./index.html', './components/**/*.{js,ts,svelte}'],
+  content: ['./index.html', './components/**/*.{js,ts,svelte}', './public/data.json'],
   theme: {},
   plugins: [],
 };
